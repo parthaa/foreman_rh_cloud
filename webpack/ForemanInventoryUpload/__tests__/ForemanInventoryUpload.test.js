0a7b6202577c3d408afbf5251618feeb88f9d537
Partha Aji <paji@redhat.com>
2025-11-06 21:36:05 -0500

    more

--- webpack/ForemanInventoryUpload/__tests__/ForemanInventoryUpload.test.js
+++ webpack/ForemanInventoryUpload/__tests__/ForemanInventoryUpload.test.js
@@ -4,9 +4,19 @@ import '@testing-library/jest-dom';
 
 import ForemanInventoryUpload from '../../ForemanInventoryUpload';
 
+jest.mock('../../Components/AccountList', () => ({
+  __esModule: true,
+  default: () => <div data-testid="account-list">AccountList</div>,
+}));
+
+jest.mock('../../Components/PageHeader', () => ({
+  __esModule: true,
+  default: () => <div data-testid="page-header">PageHeader</div>,
+}));
+
 describe('ForemanInventoryUpload', () => {
   it('should render without props', () => {
     const { container } = render(<ForemanInventoryUpload />);
-    expect(container.querySelector('.foreman-inventory-upload')).toBeInTheDocument();
+    expect(container.querySelector('.rh-cloud-inventory-page')).toBeInTheDocument();
   });
 });
